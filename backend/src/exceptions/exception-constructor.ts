7d296c5941a6da716b11d7440ecbf5f9f04082da
Calmie <safiullinrafa@yandex.ru>
2025-02-23 00:46:46 +0300

    delete doubles

--- backend/src/exceptions/exception-constructor.ts
+++
@@ -1,8 +0,0 @@
-import { HttpException } from '@nestjs/common';
-import { ErrorCode, code2message, code2status } from './error-constants';
-
-export class ServerException extends HttpException {
-  constructor(code: ErrorCode) {
-    super(`${code2message.get(code)}`, Number(code2status.get(code)));
-  }
-}
